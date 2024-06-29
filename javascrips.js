ded7a0b93f3507d9c53ee1e079bbb0958de47cd6
foca <123456789+minecraftxboxone16@gmail.com.github.com>
2024-06-29 23:09:19 +0300

    Add index.html

--- javascrips.js
+++ javascrips.js
@@ -1,71 +1,55 @@
 function getComputerChoice() {
-    let randomNumber=Math.floor(Math.random()*3)+1;
-    if(randomNumber==1) {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber == 1) {
         return "Rock";
-    } 
-    else if(randomNumber==2) {
+    } else if (randomNumber == 2) {
         return "Paper";
-    } 
-    else if(randomNumber==3) {
-        return "Scrissors";
+    } else {
+        return "Scissors";
     }
 }
-function getHumanChoice() {
-    let choice=prompt("Rock/Paper/Scrissors");
-    if(choice=='Rock')  {
-        return "Rock";
+
+let humanScore = 0;
+let computerScore = 0;
+let score = document.querySelector(".score");
+let result = document.querySelector(".result");
+
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+    if (computerChoice === humanChoice) {
+        result.textContent = `${computerChoice} is equal to ${humanChoice}. Nobody wins.`;
+    } else if (
+        (computerChoice === 'Rock' && humanChoice === 'Paper') ||
+        (computerChoice === 'Paper' && humanChoice === 'Scissors') ||
+        (computerChoice === 'Scissors' && humanChoice === 'Rock')
+    ) {
+        result.textContent = `${humanChoice} beats ${computerChoice}. You win!`;
+        humanScore++;
+    } else {
+        result.textContent = `${computerChoice} beats ${humanChoice}. You lose.`;
+        computerScore++;
     }
-    else if(choice=='Paper') {
-        return "Paper";
-    } 
-    else if(choice=='Scrissors') {
-        return "Scrissors";
+    score.textContent = `Your score: ${humanScore} || Computer score: ${computerScore}`;
+
+    if (humanScore === 5 || computerScore === 5) {
+        playGame();
     }
 }
-let humanScore=0;
-let computerScore=0;
-function playRound() {
-    let cc=getComputerChoice();
-    let hc=getHumanChoice();
-    if(cc==hc)  {
-        console.log(cc+" is equal to "+hc+".Nobody wins")
-    }
-    else if(cc=='Rock' && hc=='Paper') {
-        console.log(hc+" beats "+cc+".You win");
-        humanScore++;
-    }
-    else if(cc=='Rock' && hc=='Scrissors') {
-        console.log(cc+" beats "+hc+".You lose");
-        computerScore++;
-    }
-    else if(cc=='Paper' && hc=='Scrissors') {
-        console.log(hc+" beats "+cc+".You win");
-        humanScore++;
-    }
-    else if(cc=='Paper' && hc=='Rock') {
-        console.log(cc+" beats "+hc+".You lose");
-        computerScore++;
-    }
-    else if(cc=='Scrissors' && hc=='Paper') {
-        console.log(cc+" beats "+hc+".You lose");
-        computerScore++;
-    }
-    else if(cc=='Scrissors' && hc=='Rock') {
-        console.log(hc+" beats "+cc+".You win");
-        humanScore++;
-    }
-}
+
 function playGame() {
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    if(humanScore>computerScore) {
-        console.log("You win!");
+    if (humanScore > computerScore) {
+        result.textContent = "You win the game!";
+    } else {
+        result.textContent = "You lose the game!";
     }
-    else {
-        console.log("You lose!");
-    }
+    humanScore = 0;
+    computerScore = 0;
 }
-playGame();
+
+let buttonRock = document.querySelector(".Rock");
+let buttonPaper = document.querySelector(".Paper");
+let buttonScissors = document.querySelector(".Scissors");
+
+buttonRock.addEventListener("click", () => playRound("Rock"));
+buttonPaper.addEventListener("click", () => playRound("Paper"));
+buttonScissors.addEventListener("click", () => playRound("Scissors"));
